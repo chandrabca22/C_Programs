bf129fbb419bddf0ed4314439ded32ec0ba25bbc
Chandra Prakash <codewithcp@gmail.com>
2023-05-04 11:09:45 +0530

    New Program Added

---
+++ Question-3.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+// Now we are going to use new line character.
+printf("I\nIN\nIND\nINDI\nINDIA\nINDI\nIND\nIN\nI"); 
+// Output is
+// I
+// IN
+// IND
+// INDI
+// INDIA
+// INDI 
+// IND
+// IN 
+// I
+getch();
+}
