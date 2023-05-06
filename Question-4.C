380a5f0bb23ebe72290cf84bddd583b5c9243ffc
Chandra Prakash <codewithcp@gmail.com>
2023-05-06 17:46:45 +0530

    Show The Use Of New Tab Character

---
+++ Question-4.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+printf("Without using new tab character\n");
+printf("ChandraPrakash");
+// Output is ChandraPrakash.
+printf("\nAfter using new tab character\n");
+printf("Chandra\t\tPrakash");
+// Output is Chandra      Prakash.
+getch();
+}
