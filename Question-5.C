439b3467264999bb95c90d62ac7dbcdb1854bbaf
Chandra Prakash <codewithcp@gmail.com>
2023-05-08 18:17:56 +0530

    Show The Use Of Escape Sequences

---
+++ Question-5.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+printf("\n\nYou\n");
+printf("Are\n");
+printf("Learning\n");
+printf("'C' Language\n");
+printf("Do You Know C Language\n");
+// output - You
+//          Are
+//	    Learning
+//	    'C' Language
+//	    Do You Know C Language
+getch();
+}
