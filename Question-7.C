2096ee84556c42269a09902ab0ddd8e1e8bcc13e
Chandra Prakash <codewithcp@gmail.com>
2023-05-11 21:13:55 +0530

    Read a number and calculate square of it

---
+++ Question-7.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,sqr;
+clrscr();
+printf("Enter any number to find square : ");
+scanf("%d",&n);
+sqr=n*n;
+printf("Square of entered number is = %d",sqr);
+getch();
+}
