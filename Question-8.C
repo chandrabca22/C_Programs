353ab4a4761ddca1d4c63fe064c0b8cdaa90f74e
Chandra Prakash <codewithcp@gmail.com>
2023-05-12 21:35:16 +0530

    Read fahrenheit to change into celsius

---
+++ Question-8.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+float f,cel;
+clrscr();
+printf("Enter fahrenheit to change into celsius : ");
+scanf("%f",&f);  // for e.x - 45 fahrenheit
+// Formula
+cel=(f-32)*5/9; // it will change fahrenheit into celsius
+printf("Celsius is = %f",cel);
+// Celsius is = 7.22222
+getch();
+}
