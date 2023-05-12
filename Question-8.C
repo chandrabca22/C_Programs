3c10122e74464ff09028c0937bfd90fb3f146a1b
Chandra Prakash <codewithcp@gmail.com>
2023-05-12 21:42:05 +0530

    Read fahrenheit and change it into celsius

--- Question-8.C
+++ Question-8.C
@@ -8,7 +8,8 @@ printf("Enter fahrenheit to change into celsius : ");
 scanf("%f",&f);  // for e.x - 45 fahrenheit
 // Formula
 cel=(f-32)*5/9; // it will change fahrenheit into celsius
-printf("Celsius is = %f",cel);
-// Celsius is = 7.22222
+
+printf("Celsius is = %f",cel); // Celsius is = 7.22222
+
 getch();
 }
