99838b52384a42fa47a33bf7d986f14c3b26df17
Chandra Prakash <codewithcp@gmail.com>
2023-05-24 21:51:15 +0530

    Read Principal, Rate & Time to Calculate Simple Interest

--- Question-15.C
+++ Question-15.C
@@ -3,6 +3,10 @@
 void main()
 {
 float p,r,t,si;
+// Algorithm to Solve this Problem
+// Step 1 - Read principal, rate & time.
+// Step 2 - Apply in the formula.
+// Step 3 - Display the simple interest.
 clrscr();
 printf("\n\nEnter principal, Rate & time to calculate Simple interest\n");
 scanf("%f %f %f",&p,&r,&t);
